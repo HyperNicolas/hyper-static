9d08287564333a678fa9971e6923cca652008d61
Nicolas Jonnaert <nicolas@hypercharge.io>
2019-05-28 12:02:35 +0200

    Icons

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,11 @@ const ToolDescription = styled.div`
   color: #6b6b6b;
   padding-bottom: 0.5rem;
 `;
+const SummaryIcon = styled(Icon)`
+  font-size: 4rem;
+  margin-bottom: 1rem;
+`;
+
 const GenericContainer = styled.div`
   background: ${props => (props.background ? '#f7faff' : '#fff')};
   text-align: center;
@@ -80,22 +85,26 @@ const summary = [
   {
     title: 'Flexibility',
     description:
-      'Easily design, deploy, and evaluate new ways of working to your workforce.'
+      'Easily design, deploy, and evaluate new ways of working to your workforce.',
+    icon: 'rocket'
   },
   {
     title: 'Web-based',
     description:
-      'Naturally and automatically adjust to fit any hardware you use. Use it on the road or in the office.'
+      'Naturally and automatically adjust to fit any hardware you use. Use it on the road or in the office.',
+    icon: 'cloud'
   },
   {
     title: 'Automate',
     description:
-      'Create processes that send beautiful emails, respond to data changes, contact your legacy systems, and more.'
+      'Create processes that send beautiful emails, respond to data changes, contact your legacy systems, and more.',
+    icon: 'thunderbolt'
   },
   {
     title: 'Third-party integrations',
     description:
-      'Pull and push data from multiple sources and manage everything in one single platform, your Hyperportal.'
+      'Pull and push data from multiple sources and manage everything in one single platform, your Hyperportal.',
+    icon: 'pie-chart'
   }
 ];
 
@@ -190,8 +199,9 @@ const Index = () => (
     </Wrapper>
     <GenericContainer background>
       <Wrapper className="cp-c-row cp-c-align-spacebetween-start cp-c-wrap cp-c-padding-4">
-        {summary.map(({ title, description }) => (
+        {summary.map(({ title, description, icon }) => (
           <div key={title} className="cp-i-100 cp-i-md-50">
+            <SummaryIcon type={icon} theme="twoTone" />
             <Title>{title}</Title>
             <Description>{description}</Description>
           </div>
